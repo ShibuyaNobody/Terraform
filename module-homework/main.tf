7f18fbd1586b0c61ea7f688127de167d5eb4baa6
Raikiri <ShibuyaNobody@gmail.com>
2020-11-11 18:23:41 -0800

    This version is the full version without modules

--- module-homework/main.tf
+++ module-homework/main.tf
@@ -6,16 +6,16 @@ module "vpc_city_practice" {
   source = "github.com/CSUN-IT/TerraformModules.git/AWS/VPC/v1.0"
 
   project_name = "big-city"
-  vpc_block    = "10.1.1.0/22"
+  vpc_block    = "10.1.0.0/22"
 
   public_subnet_blocks = {
-    "10.1.1.0/24"   = "us-west-1a",
-    "10.1.1.256/24" = "us-west-1b"
+    "10.1.0.0/24"   = "us-west-1a",
+    "10.1.1.0/24" = "us-west-1b"
   }
 
   private_subnet_blocks = {
-    "10.1.1.512/24" = "us-west-1a",
-    "10.1.1.768/24" = "us-west-1b"
+    "10.1.2.0/24" = "us-west-1a",
+    "10.1.3.0/24" = "us-west-1b"
   }
 
   private_subnet_gateway = {
